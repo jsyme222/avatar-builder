98cdcdc25535f80f66dad1e4faaaacb670e2305b
jsyme222 <jsyme222@gmail.com>
2020-05-14 09:57:48 -0600

    Thumbnailer working and available options thubnails looking good

--- src/js/components/items/item.js
+++ src/js/components/items/item.js
@@ -7,7 +7,7 @@ const useStyles = makeStyles((theme) => ({
     baseOptionContainer: {
         margin: 10,
         padding: 5,
-        transition: 'all 0.5 ease',
+        transition: 'all 0.25 ease',
         '&:hover': {
             cursor: 'pointer',
             background: '#f0efed',
@@ -20,17 +20,24 @@ const useStyles = makeStyles((theme) => ({
         }
     },
     baseOptionImage: {
-        height: 100,
+        height: 50,
+        maxWidth: 100,
+    },
+    smallOptionImage: {
+        maxHeight: 15,
+        maxWidth: 50,
     },
     baseOptionEquipped: {
         margin: 10,
-        padding: 5,
+        padding: 10,
+        background: 'rgba(0, 0, 0, 0.15)',
         boxShadow: `2px 2px 10px -10px ${theme.palette.primary.main},0px 1px 1px 0px ${theme.palette.primary.main},0px 1px 3px 7px ${theme.palette.secondary.main}`,
+        '& >img': {
+        },
         '&:hover': {
             cursor: 'pointer',
-            background: '#f0efed',
             '& >img': {
-                padding: 0,
+                padding: 'initial',
             }
         },
     }
@@ -38,6 +45,7 @@ const useStyles = makeStyles((theme) => ({
 
 function Item(props) {
     const [equipped, setEquipped] = useState(false);
+    const [useClassName, setUseClassName] = useState(null);
     const classes = useStyles();
 
     useEffect(() => {
@@ -46,13 +54,33 @@ function Item(props) {
         }
     }, [props.equipped, ]);
 
+    useEffect(() => {
+        const getClassName = (src) => {
+            let image = new Image();
+            image.src = src;
+            image.onload = function() {
+                let w = this.width;
+                let h = this.height;
+                if(Math.max(w, h) < 50 ){
+                    setUseClassName(classes.smallOptionImage);
+                }else{
+                    setUseClassName(classes.baseOptionImage);
+                }
+            }
+            return true
+        }
+        if(!useClassName){
+            setUseClassName(getClassName(props.item.image.thumbnail))
+        }
+    }, [])
+
     return (
         <Paper
             className={equipped ? classes.baseOptionEquipped : classes.baseOptionContainer} 
             onClick={(event) => props.onClickAction(props.item)}
             key={props.item.id}
         >
-            <img src={fullURL(props.item.image.thumbnail || props.item.image.image)} alt={props.item.alt} className={classes.baseOptionImage} />
+            <img src={fullURL(props.item.image.thumbnail || props.item.image.image)} alt={props.item.alt} className={useClassName} />
         </Paper>
     )
 }
